293ed4e6389ee0bfec82ecd3d4062754313f213e
Wiktoria Małażewska <011987214@pw.edu.pl>
2025-01-04 19:25:20 +0100

    changed functions in monsters classes

--- monsters.py
+++ monsters.py
@@ -12,13 +12,16 @@ class Monster_level_1:
             else:
                 raise ValueError('Monsters health has to be positive')
             self.name = monster['name']
-            self.item = monster['item']
+            if monster['coins'] < 0:
+                raise ValueError('Number of coins can not be negative')
+            else:
+                self.coins = monster['coins']
             self.description = monster['description']
         except KeyError:
             raise KeyError('The dictionary should contain keys:'
-                           'type, level, health, name, item, description')
+                           'type, level, health, name, coins, description')
 
-    def get_damage(self, damage: int):
+    def get_damage(self, damage: int) -> None:
         if damage < 10:
             raise ValueError('Damage must be positive')
         if self.is_alive(damage):
@@ -26,21 +29,19 @@ class Monster_level_1:
         else:
             self.die()
 
-    def give_item(self):
-        if self.item is not None:
-            return self.item
+    def give_coins(self) -> int:
+        return self.coins
 
-    def is_alive(self, damage: int):
+    def is_alive(self, damage: int) -> int:
         return ((self.health-damage) > 0)
 
-    def die(self):
-        self.give_item()
+    def die(self) -> None:
+        self.give_coins()
         self.health = 0
-        # usuwanie z mapy
 
 
 class Monster_level_2(Monster_level_1):
-    def __init__(self, monster: dict):
+    def __init__(self, monster: dict) -> None:
         super().__init__(monster)
         try:
             if monster['hit strength'] > 0:
@@ -50,26 +51,29 @@ class Monster_level_2(Monster_level_1):
         except KeyError:
             raise KeyError('The dictionary should contain key: hit strength')
 
-    def attack_player(self):
-        return self.hit_strength
+    def attack_player(self) -> int:
+        if randint(1, 4) == 4:
+            return self.hit_strength
+        else:
+            return 0
 
 
 class Monster_level_3(Monster_level_2):
-    def __init__(self, monster: dict):
+    def __init__(self, monster: dict) -> None:
         super().__init__(monster)
 
-    def heal(self, damage):
+    def heal(self, damage) -> None:
         if damage < 0:
             raise ValueError('Damage must be a positive int')
         if randint(1, 4) == 4:
             if self.health + damage < self._max_health:
                 self.health += damage
-                return 'The monster has healed!'
             else:
                 self.health = self._max_health
 
-    def attack_player(self):
+    def attack_player(self) -> None:
         if randint(1, 4) == 4:
-            return self.hit_strength*2
-        else:
-            return self.hit_strength
+            if randint(1, 4) == 4:
+                return self.hit_strength*2
+            else:
+                return self.hit_strength
