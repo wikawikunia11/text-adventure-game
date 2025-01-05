416b671519ffb12af08ae86ac6a68732de55114b
Wiktoria Małażewska <011987214@pw.edu.pl>
2025-01-05 16:34:14 +0100

    added testing for class player and changed functions accordingly

--- player.py
+++ player.py
@@ -1,19 +1,34 @@
 import json
-from world import World
-from location import Location
-from monsters import Monster_level_1, Monster_level_2, Monster_level_3
-from items import Trader, Backpack
+from items import Backpack
 from clear_console import clear_terminal
 
 
 class Player:
     def __init__(self, file_name: str) -> None:
-        with open(file_name, 'r') as file:
-            data = json.loads(file)
-            self.health = data["health"],
-            self.hit_strength = data["hit_strength"],
-            self.backpack = Backpack(data['backpack']['coins'])
-            self.location = (data["location"]['x'], data["location"]['y'])
+        try:
+            with open(file_name, 'r') as file:
+                data = json.load(file)
+                try:
+                    if data["health"] <= 0:
+                        raise ValueError('Health must be positive')
+                    else:
+                        self.health = data["health"]
+                        self._max_health = data["health"]
+                    if data["hit_strength"] < 0:
+                        raise ValueError('Hit strength must be positive')
+                    else:
+                        self.hit_strength = data["hit_strength"]
+                    self.backpack = Backpack(data['backpack']['coins'])
+                    try:
+                        self.location = (data["location"]["x"],
+                                         data["location"]["y"])
+                    except KeyError:
+                        raise KeyError('Location should have keys: x and y')
+                except KeyError:
+                    raise KeyError('The dictionary should contain: health,'
+                                   'hit_strength, backpack, location')
+        except FileNotFoundError:
+            raise FileNotFoundError('There is no such file')
 
     def is_alive(self, damage: int) -> bool:
         return (self.health - damage > 0)
@@ -32,13 +47,11 @@ class Player:
         # od poczatku
 
     def heal(self) -> None:
-        with open("player.json", 'r') as file:
-            data = json.loads(file)
-            self.health = data["health"]
+        self.health = self._max_health
 
-    def give_options(self, current_loc: Location) -> list:
+    def give_options(self, current_loc: object) -> list:
         moves_list = []
-        if current_loc.is_monster:
+        if current_loc.is_monster():
             if current_loc.object.is_alive(0):
                 moves_list = [move
                               for move in current_loc.moves['monster alive']]
@@ -49,7 +62,7 @@ class Player:
             moves_list = [move for move in current_loc.moves]
         return moves_list
 
-    def decision(self, current_loc: Location, world: World) -> None:
+    def decision(self, current_loc: object, world: object) -> None:
         list_of_moves = self.give_options(current_loc)
         print(list_of_moves)
         decision = input("Please write your choice: ").lower()
@@ -70,45 +83,52 @@ class Player:
             if decision == 'talk to trader':
                 self.talk_to_trader(current_loc.object)
 
-    def find_location(self, coords: tuple, location_list: list) -> Location:
-        cnt = 0
-        while coords != location_list[cnt].coordinates:
-            loc = location_list[cnt]
-            cnt += 1
-        if coords != loc.coordinates:
+    def find_location(self, coords: list, location_list: list) -> object:
+        loc = None
+        for i in range(len(location_list)):
+            if coords == location_list[i].coordinates:
+                loc = location_list[i]
+                return loc
+        if loc is None:
             raise IndexError("Location with coordinates not found")
-        else:
-            return loc
-
-    def go_east(self, world: object) -> None:
-        new_loc = self.location[0] - 1
-        if new_loc < 0:
-            self.location[0] = world.size - 1
-        else:
-            self.location[0] = new_loc
-        curr_loc = self.find_location()
-        print(curr_loc.description)
 
     def go_west(self, world: object) -> None:
+        new_loc = self.location[0] - 1
+        if new_loc < 0:
+            self.location = (world.size - 1, self.location[1])
+        else:
+            self.location = (new_loc, self.location[1])
+        curr_loc = self.find_location(self.location, world.location_list)
+        print(curr_loc.description)
+
+    def go_east(self, world: object) -> None:
         new_loc = self.location[0] + 1
         if new_loc > world.size - 1:
-            self.location[0] = 0
+            self.location = (0, self.location[1])
         else:
-            self.location[0] = new_loc
-        coordinates = (self.location[0], self.location[1])
-        curr_loc = self.find_location(coordinates, world.location_list)
+            self.location = (new_loc, self.location[1])
+        curr_loc = self.find_location(self.location, world.location_list)
         print(curr_loc.description)
 
     def go_north(self, world: object) -> None:
         new_loc = self.location[1] - 1
         if new_loc < 0:
-            self.location[1] = world.size - 1
+            self.location = (self.location[0], world.size - 1)
         else:
-            self.location[1] = new_loc
-        curr_loc = self.find_location()
+            self.location = (self.location[0], new_loc)
+        curr_loc = self.find_location(self.location, world.location_list)
         print(curr_loc.description)
 
-    def curr_loc_description(curr_loc: Location) -> None:
+    def go_south(self, world: object) -> None:
+        new_loc = self.location[1] + 1
+        if new_loc > world.size - 1:
+            self.location = (self.location[0], 0)
+        else:
+            self.location = (self.location[0], new_loc)
+        curr_loc = self.find_location(self.location, world.location_list)
+        print(curr_loc.description)
+
+    def curr_loc_description(curr_loc: object) -> None:
         clear_terminal()
         if curr_loc.is_monster():
             if curr_loc.object.is_alive(0):
@@ -118,20 +138,11 @@ class Player:
         else:
             print(curr_loc.description)
 
-    def go_south(self, world: object) -> None:
-        new_loc = self.location[1] + 1
-        if new_loc > world.size - 1:
-            self.location[1] = 0
-        else:
-            self.location[1] = new_loc
-        curr_loc = self.find_location()
-        self.curr_loc_description(curr_loc)
-
     def fight_status(self, monster: object):
         print(f'Your hp is: {self.health}.'
               f'Monsters hp is: {monster.health}.')
 
-    def fight_monster(self, monster: Monster_level_1) -> None:
+    def fight_monster(self, monster: object) -> None:
         print(f'You are fighting monster level 1 named {monster.name}.')
         while monster.is_alive(0) and self.is_alive(0):
             self.fight_status(monster)
@@ -141,7 +152,7 @@ class Player:
             print('You have defeated the monster. You are healed.')
             self.heal()
 
-    def fight_monster_level_2(self, monster: Monster_level_2) -> None:
+    def fight_monster_level_2(self, monster: object) -> None:
         print(f'You are fighting monster level 2 named {monster.name}.')
         while monster.is_alive(0) and self.is_alive(0):
             self.fight_status(monster)
@@ -152,7 +163,7 @@ class Player:
             print('You have defeated the monster. You are healed.')
             self.heal()
 
-    def fight_monster_level_3(self, monster: Monster_level_3) -> None:
+    def fight_monster_level_3(self, monster: object) -> None:
         print(f'You are fighting monster level 3 named {monster.name}.')
         while monster.is_alive(0) and self.is_alive(0):
             self.fight_status(monster)
@@ -164,7 +175,7 @@ class Player:
             print('You have defeated the monster. You are healed.')
             self.heal()
 
-    def talk_to_trader(self, trader: Trader) -> None:
+    def talk_to_trader(self, trader: object) -> None:
         trader.introduce()
 
     def receive_coins(self, coins: int) -> None:
--- test_player.py
+++ test_player.py
@@ -1,12 +1,123 @@
-import json
-from items import Backpack
+from player import Player
+from location import Location
+from world import World
+import pytest
 
 
-with open("locations.json", 'r') as file:
-    data = json.load(file)
-    loc = data['locations']
-    print(loc[6]['description']['monster dead'])
+def test_file():
+    Player("player.json")
 
-    items = [{"name": "milk"}, {"name": "cheese"}]
-    backpack = Backpack(items, 10)
-    backpack.check_items()
+
+def test_create_player():
+    player = Player("test_player.json")
+    assert player.health == 100
+    assert player.backpack.coins == 10
+    assert player.location == (0, 0)
+    assert player.hit_strength == 20
+
+
+def test_create_player_wrong_file():
+    with pytest.raises(FileNotFoundError):
+        Player("xxx.json")
+
+
+def test_player_get_damage():
+    player = Player("test_player.json")
+    player.get_damage(10)
+    assert player.health == 90
+    assert player._max_health == 100
+
+
+def test_player_heal():
+    player = Player("test_player.json")
+    player.get_damage(20)
+    player.heal()
+    assert player.health == 100
+
+
+def test_player_give_options_monster_alive():
+    player = Player("test_player.json")
+    object = {"monster": {
+          "name": "Thor",
+          "health": 200,
+          "level": 1,
+          "coins": 10,
+          "description": "This is Thor",
+          "hit_strength": 20
+        }}
+    loc = Location(1, 1, "swamp", "this is swamp",
+                   {'monster alive': ["go west", "go east"],
+                    'Monster dead': ['go east']}, object)
+    assert player.give_options(loc) == ["go west", "go east"]
+
+
+def test_player_give_options_monster_dead():
+    player = Player("test_player.json")
+    object = {"monster": {
+          "name": "Thor",
+          "health": 1,
+          "level": 1,
+          "coins": 10,
+          "description": "This is Thor",
+          "hit_strength": 20
+        }}
+    loc = Location(1, 1, "swamp", "this is swamp",
+                   {'monster alive': ["go west", "go east"],
+                    'monster dead': ['go east']}, object)
+    loc.object.get_damage(1)
+    assert player.give_options(loc) == ["go east"]
+
+
+def test_player_give_options_no_monster():
+    player = Player("test_player.json")
+    loc = Location(1, 1, "swamp", "this is swamp", ["go west", "go east"])
+    assert player.give_options(loc) == ["go west", "go east"]
+
+
+def test_player_decision():
+    # jak??
+    pass
+
+
+def test_player_go_east():
+    player = Player("test_player.json")
+    world = World("test_world.json")
+    player.go_east(world)
+    assert player.location == (1, 0)
+    player.go_east(world)
+    assert player.location == (2, 0)
+    player.go_east(world)
+    assert player.location == (0, 0)
+
+
+def test_player_go_west():
+    player = Player("test_player.json")
+    world = World("test_world.json")
+    player.go_west(world)
+    assert player.location == (2, 0)
+    player.go_west(world)
+    assert player.location == (1, 0)
+    player.go_west(world)
+    assert player.location == (0, 0)
+
+
+def test_player_go_north():
+    player = Player("test_player.json")
+    world = World("test_world.json")
+    player.go_north(world)
+    assert player.location == (0, 2)
+    player.go_north(world)
+    assert player.location == (0, 1)
+    player.go_north(world)
+    assert player.location == (0, 0)
+
+
+def test_player_go_south():
+    player = Player("test_player.json")
+    world = World("test_world.json")
+    player.go_south(world)
+    assert player.location == (0, 1)
+    player.go_south(world)
+    assert player.location == (0, 2)
+    player.go_south(world)
+    assert player.location == (0, 0)
